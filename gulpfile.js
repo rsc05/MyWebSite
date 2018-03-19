6b9bda8d8897e94eb86808da117c3413ef4c6750
Rami <rami.chehab@outlook.com>
2018-03-19 21:28:18 +0000

    new pic

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ fs = require("fs"),
 autoprefixer=require('autoprefixer'),
 cssvars=require('postcss-simple-vars'),
 nested=require('postcss-nested'),
-cssImport=require('postcss-import');
+cssImport=require('postcss-import'),
+browserSync=require('browser-sync').create();
 
 
 gulp.task('default', function() {
@@ -27,6 +28,17 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function (argument) {
 	// body...
+	browserSync.init({
+		server:{
+			baseDir: "app"
+		}
+	});
+
+	watch('./app/index.html', function () {
+		// body...
+		browserSync.reload();
+	});
+
 	watch('./app/index.html', function () {
 		// body...
 		gulp.start('html');
@@ -34,6 +46,13 @@ gulp.task('watch', function (argument) {
 
 	watch('./app/assets/styles/**/*.css', function () {
 		// body...
-		gulp.start('styles');
+		gulp.start('cssInject');
 	});
+
+});
+
+gulp.task('cssInject', ['styles'], function () {
+	// body...
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(browserSync.stream());
 });
